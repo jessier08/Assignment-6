de775f8c0615431ef403a42ce5a0d961ad93f080
Jessie Richards <jessierichards@Jessies-MacBook-Pro-2.local>
2015-11-30 15:54:09 -0500

    hmmm

--- script/script.js
+++ script/script.js
@@ -45,21 +45,14 @@ queue()
                 y:centroid[1]
             }
         });
+        
         console.log(data);
 
 		var nodes = map.selectAll('.state')
-            .data(data, function(d){return d.state});
-
-        //Represent as a cartogram of populations
-        var nodesEnter = nodes.enter()
+            .data(data, function(d){return d.state})
+            .enter()
             .append('g')
             .attr('class','state');
-        
-        nodes.exit().remove();
-        nodes
-            .attr('transform',function(d){
-                return 'translate('+d.x+','+d.y+')';
-            })
         nodes
             .append('circle')
             .attr('r',function(d){
@@ -74,68 +67,75 @@ queue()
         nodes
             .append('text')
             .text(function(d){
-                return d.name;
+                return d.fullName;
             })
-            .attr('text-anchor','middle');
+            .attr('text-anchor','middle')
+            .attr('transform','translate('+0+','+4+')');
 
         //TODO: create a force layout
         //with what physical parameters?
         var force = d3.layout.force()
             .size([width,height])
-            .charge(50)
-            .gravity(0);
+            .charge(-50)
+            .gravity(.01);
 
         force.nodes(data)
-            .on('tick', function(e){
-                    var q = d3.geom.quadtree(data),
-                        i = 0,
-                        n = data.length;
-
-                    while( ++i<n ){
-                        q.visit(collide(data[i]));
-                    }
-
-                    nodes
-                        .each(gravity(e.alpha*.1))
-                        .attr('cx',function(d){return d.x})
-                        .attr('cy',function(d){return d.y})
-
-                    function gravity(k){
-                        //custom gravity: data points gravitate towards a straight line
-                        return function(d){
-                            d.y += (height/2 - d.y)*k;
-                            d.x += (d.x0 - d.x)*k;
-                        }
-                    }
-
-                    function collide(dataPoint){
-                        var nr = dataPoint.r + 5,
-                            nx1 = dataPoint.x - nr,
-                            ny1 = dataPoint.y - nr,
-                            nx2 = dataPoint.x + nr,
-                            ny2 = dataPoint.y + nr;
-
-                        return function(quadPoint,x1,y1,x2,y2){
-                            if(quadPoint.point && (quadPoint.point !== dataPoint)){
-                                var x = dataPoint.x - quadPoint.point.x,
-                                    y = dataPoint.y - quadPoint.point.y,
-                                    l = Math.sqrt(x*x+y*y),
-                                    r = nr + quadPoint.point.r;
-                                if(l<r){
-                                    l = (l-r)/l*.1;
-                                    dataPoint.x -= x*= (l*.05);
-                                    dataPoint.y -= y*= l;
-                                    quadPoint.point.x += (x*.05);
-                                    quadPoint.point.y += y;
-                                }
-                            }
-                            return x1>nx2 || x2<nx1 || y1>ny2 || y2<ny1;
-                        }
-                    }
-            })
+            .on('tick', onForceTick)
             .start();
 
+        function onForceTick(e){
+            var q = d3.geom.quadtree(data),
+                i = 0,
+                n = data.length;
+
+            // while( ++i<n ){
+            //     q.visit(collide(data[i]));
+            // }
+
+            nodes
+                .each(gravity(e.alpha*.05))
+                .attr('transform',function(d){
+                        return 'translate('+d.x+','+d.y+')';
+                    })
+                // .attr('cx',function(d){return d.x})
+                // .attr('cy',function(d){return d.y})
+
+            function gravity(k){
+                //custom gravity: data points gravitate towards a straight line
+                return function(d){
+                    d.y += (d.y0 - d.y)*k;
+                    d.x += (d.x0 - d.x)*k;
+                }
+            }
+
+            // function collide(dataPoint){
+            //     var nr = dataPoint.r + 5,
+            //         nx1 = dataPoint.x - nr,
+            //         ny1 = dataPoint.y - nr,
+            //         nx2 = dataPoint.x + nr,
+            //         ny2 = dataPoint.y + nr;
+
+            //     return function(quadPoint,x1,y1,x2,y2){
+            //         if(quadPoint.point && (quadPoint.point !== dataPoint)){
+            //             var x = dataPoint.x - quadPoint.point.x,
+            //                 y = dataPoint.y - quadPoint.point.y,
+            //                 l = Math.sqrt(x*x+y*y),
+            //                 r = nr + quadPoint.point.r;
+            //             if(l<r){
+            //                 l = (l-r)/l*.1;
+            //                 dataPoint.x -= x*= (l*.05);
+            //                 dataPoint.y -= y*= l;
+            //                 quadPoint.point.x += (x*.05);
+            //                 quadPoint.point.y += y;
+            //             }
+            //         }
+            //         return x1>nx2 || x2<nx1 || y1>ny2 || y2<ny1;
+            //     }
+            // }
+        }
 	});
+
+
 
 function parseData(d){
     //Use the parse function to populate the lookup table of states and their populations/% pop 18+
