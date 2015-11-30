77033bd816d43bd5d63a57c5cf5ae5c500c27f1d
Jessie Richards <jessierichards@Jessies-MacBook-Pro-2.local>
2015-11-30 13:19:17 -0500

    BLAH

--- script/script.js
+++ script/script.js
@@ -54,8 +54,8 @@ queue()
         var nodesEnter = nodes.enter()
             .append('g')
             .attr('class','state');
+        
         nodes.exit().remove();
-
         nodes
             .attr('transform',function(d){
                 return 'translate('+d.x+','+d.y+')';
@@ -81,7 +81,59 @@ queue()
         //TODO: create a force layout
         //with what physical parameters?
         var force = d3.layout.force()
-        //on "tick" event ...
+            .size([width,height])
+            .charge(50)
+            .gravity(0);
+
+        force.nodes(data)
+            .on('tick', function(e){
+                    var q = d3.geom.quadtree(data),
+                        i = 0,
+                        n = data.length;
+
+                    while( ++i<n ){
+                        q.visit(collide(data[i]));
+                    }
+
+                    nodes
+                        .each(gravity(e.alpha*.1))
+                        .attr('cx',function(d){return d.x})
+                        .attr('cy',function(d){return d.y})
+
+                    function gravity(k){
+                        //custom gravity: data points gravitate towards a straight line
+                        return function(d){
+                            d.y += (height/2 - d.y)*k;
+                            d.x += (d.x0 - d.x)*k;
+                        }
+                    }
+
+                    function collide(dataPoint){
+                        var nr = dataPoint.r + 5,
+                            nx1 = dataPoint.x - nr,
+                            ny1 = dataPoint.y - nr,
+                            nx2 = dataPoint.x + nr,
+                            ny2 = dataPoint.y + nr;
+
+                        return function(quadPoint,x1,y1,x2,y2){
+                            if(quadPoint.point && (quadPoint.point !== dataPoint)){
+                                var x = dataPoint.x - quadPoint.point.x,
+                                    y = dataPoint.y - quadPoint.point.y,
+                                    l = Math.sqrt(x*x+y*y),
+                                    r = nr + quadPoint.point.r;
+                                if(l<r){
+                                    l = (l-r)/l*.1;
+                                    dataPoint.x -= x*= (l*.05);
+                                    dataPoint.y -= y*= l;
+                                    quadPoint.point.x += (x*.05);
+                                    quadPoint.point.y += y;
+                                }
+                            }
+                            return x1>nx2 || x2<nx1 || y1>ny2 || y2<ny1;
+                        }
+                    }
+            })
+            .start();
 
 	});
 
